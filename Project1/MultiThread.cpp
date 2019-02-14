afdb62907d7376d339c4bf24b811145df2bf4b62
ry-valdriz <ryvaldriz101@gmail.com>
2019-02-13 23:30:02 -0800

    2d arrays

--- Project1/MultiThread.cpp
+++ Project1/MultiThread.cpp
@@ -18,9 +18,9 @@ void *matrix_mult(void *t) {
 }
 
 int main (int argc, char *argv[]) {
-  int sizeOfArray = 9;
-  int a1[sizeOfArray];
-  int a2[sizeOfArray];
+  // int sizeOfArray = 9;
+  // int a1[sizeOfArray];
+  // int a2[sizeOfArray];
   int arr1[3][3];
   int arr2[3][3];
 
@@ -41,40 +41,56 @@ int main (int argc, char *argv[]) {
   scanner2.openFile(inFileName2);
 
 
-  int i = 0;
+  // int i = 0;
   int row = 0;
   int column = 0;
 
-  while (scanner1.hasNext()) { //1d array
-    a1[i] = stoi(scanner1.next());
-    i++;
+  while(scanner1.hasNext()){
+    arr1[row][column] = scanner1.nextInt();
+    column = (column+1)%3;
+    if (column ==0){
+        row++;
+    }
   }
 
-  i = 0; //copy a1 to 2d array arr1
-  while(i<sizeOfArray){
-  arr1[row][column] = a1[i];
-  column = (column+1)%3;
-  if (column ==0){
-      row++;
-  }
-  i++;
+  while(scanner2.hasNext()){
+    arr2[row][column] = scanner2.nextInt();
+    column = (column+1)%3;
+    if (column ==0){
+        row++;
+    }
   }
 
+  // while (scanner1.hasNext()) { //1d array
+  //   a1[i] = stoi(scanner1.next());
+  //   i++;
+  // }
 
-  i=0;
-  while(scanner2.hasNext()){//1d array
-    a2[i] = stoi(scanner2.next());
-    i++;
-  }
+  //  i = 0; //copy a1 to 2d array arr1
+  // while(i<sizeOfArray){
+  // arr1[row][column] = a1[i];
+  // column = (column+1)%3;
+  // if (column ==0){
+  //     row++;
+  // }
+  // i++;
+  // }
 
-  while(i<sizeOfArray){//copy a2 into 2d array
-  arr2[row][column] = a2[i];
-  column = (column+1)%3;
-  if (column ==0){
-      row++;
-  }
-  i++;
-  }
+
+  // i=0;
+  // while(scanner2.hasNext()){//1d array
+  //   a2[i] = stoi(scanner2.next());
+  //   i++;
+  // }
+
+  // while(i<sizeOfArray){//copy a2 into 2d array
+  // arr2[row][column] = a2[i];
+  // column = (column+1)%3;
+  // if (column ==0){
+  //     row++;
+  // }
+  // i++;
+  // }
 
   // cout << "Array 1: " ;
   // for(int x = 0; x < 9; x++){ //check if scanning documents correctly
