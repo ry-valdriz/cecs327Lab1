ee34f3de6ab60cd5c358d83626856972219d6e21
Christopher Imantaka <30333920+chriztaka1997@users.noreply.github.com>
2019-02-14 16:25:57 -0800

    Uncompleted

--- Project1/MultiThread.cpp
+++ Project1/MultiThread.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <pthread.h>
 #include <unistd.h>
-#include <string.h>
+#include <string>
 
 using namespace std;
 
@@ -12,57 +12,100 @@ using namespace std;
 
 #define NUM_THREADS 9
 
+int first_input[3][3];
+int second_input[3][3];
+int result_matrix[9];
 
 void *matrix_mult(void *t) {
-
+    long r = (long)t;
+    long column = r%3;
+    
+    if(r<3){
+        int first = first_input[0][0]*second_input[0][column];
+        int second = first_input[0][1]*second_input[1][column];
+        int third = first_input[0][2]*second_input[2][column];
+        result_matrix[r]= first+second+third;
+    }
+    else if(r<6){
+        int first = first_input[1][0]*second_input[0][column];
+        int second = first_input[1][1]*second_input[1][column];
+        int third = first_input[1][2]*second_input[2][column];
+        result_matrix[r]= first+second+third;
+    }
+    else{
+        int first = first_input[2][0]*second_input[0][column];
+        int second = first_input[2][1]*second_input[1][column];
+        int third = first_input[2][2]*second_input[2][column];
+        result_matrix[r]= first+second+third;
+    }
+    
+    return 0;
 }
 
 int main (int argc, char *argv[]) {
-  int arr1[9];
-  int arr2[9];
-
-  string inFileName1 = "";
-  string inFileName2 = "";
-  ofstream outStream;
-
-  Scanner scanner1;
-  Scanner scanner2;
-
-  string element = "";
-
-  Utils::CheckArgs(2, argc, argv, "infilename1 (first matrix) infilename2 (second matrix)");
-  inFileName1 = static_cast<string>(argv[1]);
-  inFileName2 = static_cast<string>(argv[2]);
-
-  scanner1.openFile(inFileName1);
-  scanner2.openFile(inFileName2);
-
-
-  int i = 0;
-
-  while (scanner1.hasNext()) {
-    arr1[i] = stoi(scanner1.next());
-    i++;
-  }
-
-  i=0;
-  while(scanner2.hasNext()){
-    arr2[i] = stoi(scanner2.next());
-    i++;
-  }
-
-  cout << "Array 1: " ;
-  for(int x = 0; x < 9; x++){ //check if scanning documents correctly
-    cout << arr1[x] << " ";
-  }
-
-  cout<< "\nArray 2: ";
-  for(int x = 0; x < 9; x++){
-    cout << arr2[x] << " ";
-  }
-
-  cout << ""<<endl;
-  return 0;
-
-
+    string inFileName1 = "";
+    string inFileName2 = "";
+    ofstream outStream;
+    
+    Scanner scanner1;
+    Scanner scanner2;
+    
+    Utils::CheckArgs(2, argc, argv, "infilename (first matrix) outfilename (second matrix)");
+    inFileName1 = static_cast<string>(argv[1]);
+    inFileName2 = static_cast<string>(argv[2]);
+    
+    scanner1.openFile(inFileName1);
+    scanner2.openFile(inFileName2);
+    
+    
+    int row =0;
+    int column =0;
+    while (scanner1.hasNext()) {
+        first_input[row][column] = scanner1.nextInt();
+        column = (column+1)%3;
+        if (column ==0){
+            row++;
+        }
+    }
+    
+    //reset the value of row and column
+    row =0;
+    column = 0;
+    while (scanner2.hasNext()) {
+        second_input[row][column] =  scanner2.nextInt();
+        column = (column+1)%3;
+        if (column ==0){
+            row++;
+        }
+    }
+    
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    void *status;
+    
+    for(int i= 0; i < NUM_THREADS; i++){
+        
+        rc = pthread_create(&threads[i], NULL, matrix_mult, (void *)i);
+        if (rc) {
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+        }
+    }
+    for( int k = 0; k < NUM_THREADS; k++ ) {
+        rc = pthread_join(threads[k], &status);
+        if (rc) {
+            cout << "Error:unable to join," << rc << endl;
+            exit(-1);
+        }
+        cout << "Main: completed thread id :" << k ;
+        cout << "  exiting with status :" << status << endl;
+    }
+    sleep(1);
+    
+    for(int i= 0; i<NUM_THREADS;i++){
+        cout<<result_matrix[i]<<endl;
+    }
+    
+    return 0;
 }
+
